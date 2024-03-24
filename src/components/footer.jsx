a516847d0c68e7d574ec5adf115e6a43ca50844c
cassandra18 <cassandralelei013@gmail.com>
2024-03-24 22:02:10 +0300

    added content to the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,36 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      
+      <div>
+        <h2> Follow me on:</h2>
+        
+          <div>
+            {/* GitHub */}
+            <a
+              href="https://github.com/cassandra18"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-3"
+            >
+              <button className="icon-button" style={{ backgroundColor: "transparent", color: "#05FFC9"}}>
+                <FaGithub  size={24}/>
+              </button>
+            </a>
+
+            {/* LinkedIn */}
+            <a
+              href="https://www.linkedin.com/in/cassandra-lelei-88987a269"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-4"
+            >
+              <button className="icon-button" >
+                <FaLinkedin size={24}/>
+              </button>
+            </a>
+          </div>
+
+      </div>
     </div>
   )
 }
