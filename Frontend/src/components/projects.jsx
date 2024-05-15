71c38591ad036b59ee71dc286862335a5d7a745b
cassandra18 <cassandralelei013@gmail.com>
2024-05-15 09:08:55 +0300

    added the first project to the projects section

--- Frontend/src/components/projects.jsx
+++ Frontend/src/components/projects.jsx
@@ -1,15 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Projects() {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const images = [
+    {
+      src: 'images/img3.png',
+      title: "SingleBlog",
+      description: "This is the view of the single blog page in medium devices",
+    },
+    {
+      src: 'images/img2.png',
+      title: " Dashboard",
+      description: "This is the view of the dashboard in medium devices",
+    },
+    {
+      src: '/images/img1.png',
+      title: " Herosection",
+      description: "This is the view of the landing page in medium devices",
+    },
+    {
+      src: 'images/img4.png',
+      title: "AllBlogs",
+      description:
+        "This is the view of the all-blogs page in small devices with the navbar opened",
+    },
+    {
+      src: 'images/img5.png',
+      title: "Contact",
+      description:
+        "This is the view of the contact page in medium devices",
+    },
+   
+  ];
+
   return (
-    <div>
-      <>
+    <div className="relative ">
       <h1 className="md:text-4xl md:px-16  text-3xl text-white px-8 font-bold mt-12 mb-8">
         Latest <span className="text-orange-500">Project</span>
       </h1>
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'></div>
-      </>
+      <div className="w-full h-full flex items-center justify-center ">
+        <div className="flex items-center  slider-images">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`slider-img ${activeIndex === index ? 'active' : ''}`}
+              onClick={() => setActiveIndex(index)}
+            >
+              <img src={image.src} alt={image.title} />
+              <h1 className="image-h1">{image.title}</h1>
+              <div className="details">
+                <h2>{image.title}</h2>
+                <p>{image.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
