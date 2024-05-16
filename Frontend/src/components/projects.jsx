acb20028e1941999d9ecfbe353ca1a2e6bfa65f8
cassandra18 <cassandralelei013@gmail.com>
2024-05-16 08:45:50 +0300

    styled the project section

--- Frontend/src/components/projects.jsx
+++ Frontend/src/components/projects.jsx
@@ -34,19 +34,27 @@ export default function Projects() {
    
   ];
 
+  const handleImageClick = (index) => {
+    if (activeIndex === index) {
+      setActiveIndex(null);
+    } else {
+      setActiveIndex(index);
+    }
+  };
+
   return (
-    <div className="relative ">
-      <h1 className="md:text-4xl md:px-16  text-3xl text-white px-8 font-bold mt-12 mb-8">
+    <div className="relative mb-20 ">
+      <h1 className="text-4xl text-center text-white px-8 font-bold mt-12 mb-8">
         Latest <span className="text-orange-500">Project</span>
       </h1>
 
-      <div className="w-full h-full flex items-center justify-center ">
-        <div className="flex items-center  slider-images">
+      <div className="w-full h-full flex items-center justify-center px-2 py-10">
+        <div className="flex items-center overflow-x-auto  gap-4 slider-images">
           {images.map((image, index) => (
             <div
               key={index}
               className={`slider-img ${activeIndex === index ? 'active' : ''}`}
-              onClick={() => setActiveIndex(index)}
+              onClick={() => handleImageClick(index)}
             >
               <img src={image.src} alt={image.title} />
               <h1 className="image-h1">{image.title}</h1>
